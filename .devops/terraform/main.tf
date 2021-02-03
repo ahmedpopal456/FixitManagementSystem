fbef29fa8c0d3a72b1ab39832c8a78ee1093961b
Samiha <hellosamiha@gmail.com>
2021-02-03 00:38:49 -0500

    add appsettings for function app

--- .devops/terraform/main.tf
+++ .devops/terraform/main.tf
@@ -24,10 +24,29 @@ resource "azurerm_app_service_plan" "main" {
 }
 
 resource "azurerm_function_app" "api" {
-  name = "${var.organization_name}-${var.environment_name}-${var.service_abb}-api"
+  name                       = "${var.organization_name}-${var.environment_name}-${var.service_abb}-api"
   location                   = azurerm_resource_group.main.location
   resource_group_name        = azurerm_resource_group.main.name
   app_service_plan_id        = azurerm_app_service_plan.main.id
   storage_account_name       = azurerm_storage_account.api.name
   storage_account_access_key = azurerm_storage_account.api.primary_access_key
+
+  // TEMPORARY CONFIG
+  app_settings = {
+    "IsEncrypted" : false,
+    "Values" : {
+      "AzureWebJobsStorage" : "UseDevelopmentStorage=true",
+      "FUNCTIONS_WORKER_RUNTIME" : "dotnet",
+      "FIXIT-FMS-DB-EP" : "https://jlincosmostest.documents.azure.com:443/",
+      "FIXIT-FMS-DB-KEY" : "ETlBZgyZ443pHkiywqOnMJ6OUFAtLhVCr3IxhCkVptcVcCHP0JXpglFEHqnp5drnj5UCQSUuhPZkhOgtwiIGUA==",
+      "FIXIT-FMS-DB-NAME" : "cosmostest",
+      "FIXIT-FMS-DB-FIXTABLE" : "Fixes",
+      "FIXIT-FMS-DB-FIXPLANTABLENAME" : "FixPlans",
+
+      "FIXIT-FMS-QUEUE-EP" : "https://stchend.queue.core.windows.net/queuetest",
+      "FIXIT-FMS-QUEUE-CS" : "DefaultEndpointsProtocol=https;AccountName=stchend;AccountKey=RWzJev5oocpzEqVeg4Ap2IKyxOBTgoMJw5ULVn1cFn+xDfjkZjSLOKScgRXwNK4otFMnunXKg0Pwmm6xlgFgMA==;EndpointSuffix=core.windows.net",
+      "FIXIT-FMS-QUEUE-KEY" : "RWzJev5oocpzEqVeg4Ap2IKyxOBTgoMJw5ULVn1cFn+xDfjkZjSLOKScgRXwNK4otFMnunXKg0Pwmm6xlgFgMA==",
+      "FIXIT-FMS-QUEUE-NAME" : "queuetest"
+    }
+  }
 }
