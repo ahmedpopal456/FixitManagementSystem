849fb17cac03ffc5d12ce0d3899fa3d9f3ba78b7
Samiha Rahman <hellosamiha@gmail.com>
2021-02-10 04:10:07 +0000

    Merged PR 147: Create Terraform Configuration for Fix Management System
    **HOW TO TEST:**
    *Pre-requisites:* Have terraform & azure-cli in PATH
    1. Clone repo
    2. cd .devops/terraform
    3. terraform init -backend-config=./backends/dev.hcl
    4. terraform plan -var-file ./variables/dev.tfvars
    
    terraform plan output: [tfplan.txt](https://dev.azure.com/FixIt-App/08d378fd-be72-4c80-9ce4-1a7c754efb98/_apis/git/repositories/9f5651d8-b143-46f8-acd6-200580a2891b/pullRequests/147/attachments/tfplan%20%282%29.txt)
    
    Related work items: #450

---
+++ .devops/terraform/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "azurerm" {}
+  required_version = "=0.14.5"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm",
+      version = "=2.45.1"
+    }
+  }
+}
---
+++ .devops/terraform/main.tf
@@ -0,0 +1,70 @@
+resource "azurerm_resource_group" "main" {
+  name     = "${var.organization_name}-${var.environment_name}-${var.service_abb}"
+  location = var.location_name
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "${var.organization_name}${var.environment_name}${var.service_abb}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_account" "app" {
+  for_each                 = toset(var.function_apps)
+  name                     = "${var.organization_name}${var.environment_name}${var.service_abb}${each.key}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_queue" "main" {
+  name                 = "${var.organization_name}-${var.environment_name}-${var.service_abb}-queue"
+  storage_account_name = azurerm_storage_account.main.name
+}
+
+resource "azurerm_app_service_plan" "main" {
+  name                = "${var.organization_name}-${var.environment_name}-${var.service_abb}-service-plan"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "main" {
+  for_each                   = toset(var.function_apps)
+  name                       = "${var.organization_name}-${var.environment_name}-${var.service_abb}-${each.key}"
+  location                   = azurerm_resource_group.main.location
+  resource_group_name        = azurerm_resource_group.main.name
+  app_service_plan_id        = azurerm_app_service_plan.main.id
+  storage_account_name       = azurerm_storage_account.app[each.key].name
+  storage_account_access_key = azurerm_storage_account.app[each.key].primary_access_key
+
+  app_settings = {
+    "AzureWebJobsStorage" : "UseDevelopmentStorage=false",
+    "FUNCTIONS_WORKER_RUNTIME" : "dotnet",
+    "FIXIT-FMS-DB-EP" : data.azurerm_cosmosdb_account.main.endpoint,
+    "FIXIT-FMS-DB-KEY" : data.azurerm_cosmosdb_account.main.primary_key,
+    "FIXIT-FMS-DB-NAME" : data.azurerm_cosmosdb_account.main.name,
+    "FIXIT-FMS-DB-FIXTABLE" : azurerm_cosmosdb_table.main["fixes"].name,
+    "FIXIT-FMS-DB-FIXPLANTABLENAME" : azurerm_cosmosdb_table.main["fixplans"].name,
+
+    "FIXIT-FMS-QUEUE-EP" : "https://${azurerm_storage_account.main.name}.queue.core.windows.net/${azurerm_storage_queue.main.name}",
+    "FIXIT-FMS-STORAGEACCOUNT-CS" : azurerm_storage_account.main.primary_connection_string,
+    "FIXIT-FMS-STORAGEACCOUNT-KEY" : azurerm_storage_account.main.primary_access_key,
+    "FIXIT-FMS-QUEUE-NAME" : azurerm_storage_queue.main.name
+  }
+}
+
+resource "azurerm_cosmosdb_table" "main" {
+  for_each            = var.cosmosdb_tables
+  name                = each.value
+  resource_group_name = data.azurerm_cosmosdb_account.main.resource_group_name
+  account_name        = data.azurerm_cosmosdb_account.main.name
+  throughput          = 400
+}
