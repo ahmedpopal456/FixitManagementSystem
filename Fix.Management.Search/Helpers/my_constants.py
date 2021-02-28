b4ef3accbb8b00fb4b91dc9290809770af767909
David <davidchen_98@hotmail.com>
2021-02-28 01:21:58 -0500

    #350 added constant file

---
+++ Fix.Management.Search/Helpers/my_constants.py
@@ -0,0 +1,5 @@
+# CONSTANT FILE
+
+NULL_KEYWORDS = "Please pass keywords on the query string"
+INVALID_PARAM = "Invalid Request. One or many request parameters have invalid fields"
+EMPTY_RESULT = "No templates found for "
